7864690ef7a7ca574dcb9d057fa278ada62c3284
Дима Дроздов <nordost777@bk.ru>
2025-04-10 19:57:59 +0300

    final2 done

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -4,23 +4,24 @@ import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
     @Bean
-    public static Island2 getIsland(Wood3 wood) {
+    public Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
 
     @Bean
-    public static Rabbit4 getRabbit(Duck5 duck) {
+    public Rabbit4 getRabbit(Duck5 duck) {
         return new Rabbit4(duck);
     }
 
     @Bean
-    public static Egg6 getEgg(Needle7 needle) {
+    public Egg6 getEgg(Needle7 needle) {
         return new Egg6(needle);
     }
 }
